3de80377fa87f7edd720dfe468dab824c9017eda
Jasone Garay Garcia <jgarayga@lxplus432.cern.ch>
2013-07-04 17:52:56 +0200

    Currently used version of the code.

--- Harvesting.sh
+++ Harvesting.sh
@@ -1,10 +1,62 @@
 #!/bin/zsh
 
-#if [ $SCRAM_ARCH != "slc5_ia32_gcc434" ] ; then
-#  echo "wrong platform"
-#  exit
-#fi
+export SCRAM_ARCH=slc5_amd64_gcc434
 
+. /afs/cern.ch/cms/cmsset_default.sh
+source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh
+
+source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh
+pushd /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting/CMSSW_4_2_8/src
+cmsenv
+popd
+
+#pushd /afs/cern.ch/cms/sw/${SCRAM_ARCH}/cms/dbs-client/DBS_2_0_9_patch_4-cms/lib
+#source setup.sh
+#popd
+
+. /afs/cern.ch/cms/caf/setup.sh
+
+export X509_USER_PROXY=$HOME/x509up
+# update by typing on the shell voms-proxy-init -voms cms -valid 192:00
+# voms-proxy-init --rfc -valid 19:0
+voms-proxy-info
+
+LOCK=harvesting.lock
+if [ -e $LOCK ]; then
+ echo At `date`
+ echo An update is running with pid $(cat $LOCK)
+ echo Remove the lock file $LOCK if the job crashed
+ exit
+else
+ echo $$ > $LOCK
+ echo "lock generated"
+fi
+
+basedir=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting
+bindir=$basedir/bin
+outdir=$basedir/bin/test
+castordir=/castor/cern.ch/cms/store/temp/dqm/offline/harvesting_output/
+cd $bindir
+pwd
+
+####
+# remove previous stuff
+echo "cleaning previous jobs"
+rm -fr $basedir/CMSSW*/harvesting_area/*__DQM_*site_* 
+rm -fr $basedir/CMSSW*/harvesting_area/crab*
+rm -fr $basedir/CMSSW*/harvesting_area/harvesting* 
+rm -fr $basedir/CMSSW*/harvesting_area/multicrab*
+echo "cleaning done"
+
+#for arch mainrel in "slc5_amd64_gcc434" "CMSSW_4_" "slc5_amd64_gcc462" "CMSSW_5_"
+#for arch mainrel in "slc5_amd64_gcc462" "CMSSW_5_"
+#for arch mainrel in "slc5_amd64_gcc434" "CMSSW_4_"
+#for arch mainrel in "slc5_ia32_gcc434" "CMSSW_3_"
+for arch mainrel in "slc5_amd64_gcc462" "CMSSW_5_"
+
+do
+
+export SCRAM_ARCH=$arch
 date=`date +%y%m%d%H%M%S`
 
 echo 
@@ -13,63 +65,17 @@ echo Start Harvesting script at `date`
 echo "=========================================================="
 echo 
 
-#logfile=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting/bin/Harvesting_${date}.log
-#touch $logfile
-
-LOCK=harvesting.lock
-if [ -e $LOCK ]; then
- echo An update is running with pid $(cat $LOCK)
- echo Remove the lock file $LOCK if the job crashed
- exit
-else
- echo $$ > $LOCK
-fi
-
-. /afs/cern.ch/cms/sw/cmsset_default.sh
-cd /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting/CMSSW_3_8_6/src
-source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh
-cmsenv
-source /afs/cern.ch/cms/ccs/wm/scripts/Crab/CRAB_2_7_5_patch1/crab.sh
-pushd /afs/cern.ch/cms/sw/${SCRAM_ARCH}/cms/dbs-client/DBS_2_0_9_patch_4-cms/lib
-source setup.sh
-popd
-# setup for CAF
-. /afs/cern.ch/cms/caf/setup.sh
-
-
-# the following is useless, but does not harm
-export VO_CMS_SW_DIR=/afs/cern.ch/cms/sw
-export CMS_PATH=/afs/cern.ch/cms/sw
-. $CMS_PATH/cmsset_default.sh
-
-voms-proxy-init
-
-####
-basedir=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting
-bindir=$basedir/bin
-outdir=$basedir/bin/test
-cd $bindir
-pwd
-
 outfiledate=DQM_${date}.txt
 prevfile=DQM_prev.txt
-#difffiledate=DQM_${date}.log
 todofiledate=DQM_${date}.todo
-#datafiledate=DQM_${date}.datasets
-
-####
-# remove previous stuff
-
-echo "cleaning previous jobs"
-rm -fr $basedir/CMSSW*/harvesting_area/*__DQM_*site_* 
-rm -fr $basedir/CMSSW*/harvesting_area/crab*
-rm -fr $basedir/CMSSW*/harvesting_area/harvesting* 
-rm -fr $basedir/CMSSW*/harvesting_area/multicrab*
-echo "cleaning done"
+zerobfile=zerob.txt
+datafiledate=DQM_${date}.datasets
 
 echo $outfiledate
-rm -fr $outfiledate help1 help2
+rm -fr $outfiledate help
 touch $outfiledate
+rm $todofiledate
+touch $todofiledate
 
 ####
 # run DBS
@@ -80,36 +86,136 @@ echo Checking DBS for new datasets at `date`
 echo
 echo
 
-dbs search --query="find dataset, release, dataset.tag, datatype where dataset like %/DQM and site = caf.cern.ch" file.status = valid | grep DQM >> $outfiledate
+startyear=`date +%y`
+startyear=`expr $startyear + 2000`
+startmonth=`date +%m`
+startdate=$startyear-$startmonth-`date +%d`
+i=0
+for ((  i = 0 ;  i < 1;  i++  ))
+do
+enddate=$startdate
+startmonth=`expr $startmonth - 1`
+if test $startmonth -le 0
+then
+startyear=`expr $startyear - 1`
+startmonth=`expr $startmonth + 12`
+fi
+startdate=$startyear-$startmonth-`date +%d`
+#startdate=2012-11-01
+#enddate=2012-10-20
+echo $startdate $enddate
 
+dbs search --query="find dataset, release, dataset.tag, datatype, run  where dataset=/*/*/DQM and file.numevents >0 and site=srm-eoscms.cern.ch and dataset.createdate>"$startdate" and dataset.createdate < "$enddate"" and file.status = valid | grep DQM | grep ::All | grep -v preprod | grep -v /CMSSW | grep -v HLT | grep -v HIRun2011 | grep -v Cosmic | grep $mainrel>> $outfiledate
+
+#dbs search --query="find dataset, release, dataset.tag, datatype, run where dataset=/TauPlusX/Run2012A-laserHCALskim_534p2_24Oct2012-v1/DQM and file.numevents >0 and site=srm-eoscms.cern.ch and dataset.createdate>"$startdate" and dataset.createdate < "$enddate"" and file.status = valid | grep DQM | grep ::All | grep -v preprod | grep -v /CMSSW | grep -v HLT | grep -v HIRun2011 | grep -v Cosmic | grep $mainrel>> $outfiledate
+#dbs search --query="find dataset, release, dataset.tag, datatype, run  where dataset=/*/*laserHCALskim_534p2_24Oct2012*/DQM and file.numevents >0 and site=srm-eoscms.cern.ch and dataset.createdate>"$startdate" and dataset.createdate < "$enddate"" and file.status = valid | grep DQM | grep ::All | grep -v preprod | grep -v /CMSSW | grep -v HLT | grep -v HIRun2011 | grep -v Cosmic | grep $mainrel>> $outfiledate
+#dbs search --query="find dataset, release, dataset.tag, datatype, run  where dataset=/QCD_Pt-15to30_TuneZ2star_8TeV_pythia6/Summer12-PU_S7_START52_V9-v2/DQM and file.numevents >0 and site=srm-eoscms.cern.ch" and file.status = valid | grep DQM | grep ::All | grep -v preprod | grep -v /CMSSW | grep -v HLT | grep -v HIRun2011 | grep -v Cosmic | grep $mainrel>> $outfiledate
+#dbs search --query="find dataset, release, dataset.tag, datatype, run  where dataset=/DoubleElectron/Run2012C-24Aug2012-v1/DQM and file.numevents >0 and site=srm-eoscms.cern.ch" and file.status = valid | grep DQM | grep ::All | grep -v preprod | grep -v /CMSSW | grep -v HLT | grep -v HIRun2011 | grep -v Cosmic | grep $mainrel>> $outfiledate
+#dbs search --query="find dataset, release, dataset.tag, datatype, run  where dataset=/MinBias_TuneZ2star_8TeV-pythia6/Summer12_DR53X-DEBUG_PU_S10_START53_V7A-v1/DQM and file.numevents >0 and site=srm-eoscms.cern.ch" and file.status = valid | grep DQM | grep ::All | grep -v preprod | grep -v /CMSSW | grep -v HLT | grep -v HIRun2011 | grep $mainrel>> $outfiledate
+#dbs search --query="find dataset, release, dataset.tag, datatype, run  where dataset=/*/Fall11-HLTBPh2011_START44_V9B-v*/DQM and file.numevents >0 and site=srm-eoscms.cern.ch and dataset.createdate>"$startdate" and dataset.createdate < "$enddate"" and file.status = valid | grep DQM | grep ::All | grep -v preprod | grep -v /CMSSW | grep -v HIRun2011 | grep -v Cosmics | grep $mainrel>> $outfiledate
+done
 ####
 # sort outfile
-sort -u $outfiledate > help1 
-mv help1 $outfiledate
-sort -u  $prevfile > help2
-mv help2 $prevfile
-diff $outfiledate $prevfile | awk '{print $2" "$3" "$4" "$5}' | grep DQM | sort -u > $todofiledate
+sort -u $outfiledate > help 
+mv help $outfiledate
+sort -u  $prevfile > help
+mv help $prevfile
+diff $outfiledate $prevfile | grep "<" | awk '{print $6" "$2" "$3" "$4" "$5}'  | sort -u > help
+echo `date` >> $zerobfile
 
-if [ `wc -l $outfiledate | awk '{print $1}'` -ne 0 ]; then
-rm -fr $prevfile
-sort $outfiledate > $prevfile
+#python RunList.py -j JSON.txt -d help
+
+rm help2
+cat help | awk '{print $2" "$3" "$4" "$5" "$1}' |sort -u > help2
+mv help2 help
+
+for dataset rel tag type run in `cat help | awk '{print $1" "$2" "$3" "$4" "$5}'`
+do
+checkpath=$castordir
+#checkpath=/castor/cern.ch/cms/store/temp/dqm/offline/harvesting_output/TEST
+if  test $type = "mc"
+then
+checkpath=$checkpath"mc/mc/"
+else
+checkpath=$checkpath"data/dqmoffline/"
 fi
+rellow=`echo $rel| tr '[:upper:]' '[:lower:]'`
+checkpath="$checkpath"`echo $rellow|sed 's/cmssw_//g'`
+checkpath="$checkpath"/`echo $dataset|sed 's/.\(.*\)/\1/'|sed 's/\//__/g'`
+checkpath="$checkpath"/run_"$run"/nevents
+
+#######  fix
+size=-1
+rootfile=1
+for section in `nsls $checkpath`;
+do
+sectiondir=$checkpath/$section
+for file in `nsls $sectiondir`;
+do
+echo $file | grep -q $castordir
+if [ $? -eq 0 ]
+then
+rootfile=$file
+else
+rootfile=$sectiondir/$file
+fi
+### end fix
+
+### how it was before the fix
+#checkpath="$checkpath"/`echo $dataset|sed 's/.\(.*\)/\1/'|sed 's/\//__/g'`_
+#checkpath="$checkpath"`printf %.9d run`_site_01
+#size=-1
+#rootfile=1
+#for file in `nsls $checkpath`
+#do
+#rootfile=$checkpath/$file
+### end how it was before the fix
+
+size=`rfstat $rootfile | grep Size | perl -pe 's/Size \(bytes\)    \: //'`
+if test $size -gt 0
+then
+echo $dataset   $rel   $tag   $type   $run >> $prevfile
+else
+echo $dataset   $rel   $tag   $type   $run >> $zerobfile
+fi
+done
+### fix
+done
+### end fix
+if [[ "$rootfile" == "1" ]]
+    then
+    echo $checkpath "deleted"
+    nsrm -rf $checkpath
+    echo $checkpath "deleted"
+
+fi
+if test $size -lt 0
+then
+#nsrm -rf $checkpath
+echo $dataset $rel $tag $type >> $todofiledate
+fi
+done
+rm help
+sort -u $prevfile > help
+mv help $prevfile
+sort -u $todofiledate > help
+mv help $todofiledate
 
 ###
 # if non-zero then produce todo files and all the rest
 if [ `wc -l $todofiledate | awk '{print $1}'` -ne 0 ] ; then
-cp $todofiledate $outdir/$todofiledate
+#cp $todofiledate $outdir/$todofiledate
 
-
+#rm -fr DQM_${date}.$i
 for i in `awk '{print $2}' $todofiledate | sort -u` ; do
-rm -fr DQM_${date}.$i
-sort -u $todofiledate | grep $i" " | grep "mc" | awk '{print $1" "$2" "$3" "$4}' > DQM_${date}.mc.$i
-sort -u $todofiledate | grep $i" " | grep "data" | awk '{print $1" "$2" "$3" "$4}' > DQM_${date}.data.$i
+echo $i
+more $todofiledate | grep $i" " | grep "mc" | awk '{print $1" "$2" "$3" "$4}' > DQM_${date}.mc.$i
+more $todofiledate | grep $i" " | grep "data" | awk '{print $1" "$2" "$3" "$4}' > DQM_${date}.data.$i
 done
 
 ###
 # remove zero-length files
-for file in `ls DQM_${date}.*.CMSSW*` ; do
+for file in `ls DQM_${date}.mc.CMSSW* DQM_${date}.data.CMSSW*` ; do
 count=`wc -l $file | awk '{print $1}'` 
 echo $count
 if [ $count -eq 0 ] ; then 
@@ -120,20 +226,23 @@ done
 ### 
 # copy to output
 cat DQM_${date}.*.CMSSW*
-cp DQM_${date}.*.CMSSW* $outdir/.
+#cp DQM_${date}.*.CMSSW* $outdir/.
 
+##-------------------------------------
+##-------------------------------------
+##-------------------------------------
 
 #####
 # now submit jobs
 #
 
-cd $outdir
+#cd $outdir
 
 ##-------------------------------------------
-## loop over all datasets
+## loop over all datasets in reverse alphabetical order (MC first)
 ##-------------------------------------------
 
-for i in `ls DQM_${date}.*.CMSSW*` ; do
+for i in `ls -r DQM_${date}.*.CMSSW*` ; do
 
 for dataset in `cat $i | awk {'print $1'} | uniq` ; do
 cmssw=`cat $i | grep $dataset | awk {'print $2'} | uniq`
@@ -145,6 +254,10 @@ htype="DQMoffline"
 else
 htype="MC"
 fi
+echo $dataset | grep -q preprod
+if [ $? -eq 0 ] ; then
+htype="dqmHarvestingPOG"
+fi
 
 echo $i $dataset $cmssw $tag $dtype $htype
 
@@ -154,8 +267,7 @@ if [ $cmssw = "CMSSW_3_7_0_patch3" ] ; then ; cmssw=CMSSW_3_7_0_patch4 ; fi
 if [ $cmssw = "CMSSW_3_6_2" ] ; then ; cmssw=CMSSW_3_6_3 ; fi
 
 
-## Set up CMSSW environment, if necessary 
-[ -d /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting/$cmssw ]
+[ -d $basedir/$cmssw ]
 if [ `echo $?` != 0 ];
 then
 echo 
@@ -163,14 +275,18 @@ echo "####################################################################"
 echo Setting up $cmssw at `date`
 echo 
 echo 
-cd /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting
+#cd /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting
+cd $basedir
 scramv1 project CMSSW $cmssw
 cd $cmssw/src
+cmsenv
 cvs co DQM/Integration/scripts/harvesting_tools
-cvs co -r CMSSW_3_6_1 Configuration/PyReleaseValidation/python/ConfigBuilder.py
-addpkg Configuration/StandardSequences
-cvs update -r 1.9 Configuration/StandardSequences/python/Harvesting_cff.py
-sed -i 's/postValidation\*hltpostvalidation_prod/hltpostvalidation_prod/'  Configuration/StandardSequences/python/Harvesting_cff.py
+cvs co -r 1.303 Configuration/PyReleaseValidation
+#addpkg HLTrigger/Configuration V11-03-99-23
+# cvs co -r CMSSW_3_6_1 Configuration/PyReleaseValidation/python/ConfigBuilder.py
+# addpkg Configuration/StandardSequences
+# cvs update -r 1.9 Configuration/StandardSequences/python/Harvesting_cff.py
+# sed -i 's/postValidation\*hltpostvalidation_prod/hltpostvalidation_prod/'  Configuration/StandardSequences/python/Harvesting_cff.py
 scramv1 b
 cd ..
 mkdir harvesting_area
@@ -178,8 +294,13 @@ cd harvesting_area
 ln -s ../src/DQM/Integration/scripts/harvesting_tools/cmsHarvester.py .
 ln -s ../src/DQM/Integration/scripts/harvesting_tools/check_harvesting.pl .
 fi
+cd $basedir/$cmssw/src
+cmsenv
+# rm -r ../harvesting_area/cmsHarvester.py
+# ln -s DQM/Integration/scripts/harvesting_tools/cmsHarvester.py ../harvesting_area/.
+# cp ../../bin/cmsHarvester.py ../harvesting_area/cmsHarvester.py
+scramv1 b
 
-cd /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting/$cmssw/src
 export VO_CMS_SW_DIR=/afs/cern.ch/cms/sw
 eval `scramv1 runtime -sh`
 cd ../harvesting_area
@@ -192,29 +313,25 @@ echo
 
 if [ $dtype = "data" ] ; then 
     ./cmsHarvester.py --dataset=$dataset --harvesting_type=$htype \
-	--globaltag=$tag --site=CAF --force --Jsonfile=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting/bin/JSON.txt
+	--globaltag=$tag --site=srm-eoscms.cern.ch --force --saveByLumiSection --no-t1access --castordir=$castordir \
+	# --Jsonfile=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting/bin/JSON.txt
 else
     ./cmsHarvester.py --dataset=$dataset --harvesting_type=$htype \
-	--globaltag=$tag --site=CAF --no-ref-hists --force
+	--globaltag=$tag --site=srm-eoscms.cern.ch --no-ref-hists --force --no-t1access \
+        --castordir=$castordir
 fi
 
 echo 
 echo "####################################################################" 
-echo Start creating jobs at `date`
+echo Start creating and submitting jobs at `date`
 echo 
 echo 
-multicrab -create
 
-echo
-echo "####################################################################"
-echo Start submitting jobs at `date` ... 
-echo 
-echo 
-multicrab -submit
+multicrab -create -submit
 
 rm -fr harvesting_accounting.txt	 
 
-cd $outdir	 
+cd $bindir	 
 	 
 done
 done
@@ -226,11 +343,15 @@ done
 fi
 
 cd $bindir
-rm -fr help2 help1
-rm -f $LOCK
+#rm -fr help2 help1
+#rm -f $LOCK
 
 echo 
 echo "=========================================================="
 echo End Harvesting script at `date`
 echo "=========================================================="
 echo
+done
+
+rm -fr help2 help1
+rm -f $LOCK
--- Upload.sh
+++ Upload.sh
@@ -17,8 +17,11 @@ echo "============================"
 echo "running Upload at" $date
 echo
 echo "certificate:"
-source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh
+source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env_3_2.sh
 X509_USER_PROXY=$HOME/x509up
+echo
+echo "sourcing EOS"
+source /afs/cern.ch/project/eos/installation/client/etc/setup.sh
 #
 # note: before installing cronjob, run voms-proxy-init -hours=100000 with X509_USER_PROXY set to sth not on /tmp
 #
@@ -26,31 +29,67 @@ voms-proxy-info
 
 echo 
 echo "----------------------------"
-for i in "data/dqmoffline" "mc/mc" 
+#for i in "data/dqmoffline" "mc/mc"
+for i in "mc/mc"
+#for i in "data/dqmoffline" 
 do
 echo 
 echo "==== Upload of new files in" $i
-Castordir=/castor/cern.ch/cms/store/temp/dqm/offline/harvesting_output/$i
-for CMSSW in `nsls $Castordir`;
+#Castordir=/castor/cern.ch/cms/store/temp/dqm/offline/harvesting_output/$i
+Castordir=/eos/cms/store/group/comm_dqm/harvesting_output/$i
+
+# #for CMSSW in `nsls $Castordir | grep -v 3_6_ | grep -v 3_7_ | grep -v 3_8 | grep -v 3_9 | grep -v 3_10 | grep -v 3_11 | grep -v 4_1 | grep -v hlt1`;
+# #for CMSSW in 5_2_5 5_3_3_patch3 5_3_2_patch5 5_3_2_patch4
+#for CMSSW in `eos ls $Castordir`;
+#for CMSSW in 5_3_7_patch5
+#for CMSSW in 5_3_8_patch3
+#for CMSSW in 5_3_7_alcapatch1
+#for CMSSW in 5_3_6_patch1
+#for CMSSW in 6_0_1_PostLS1v2_patch3 5_3_9_patch1 5_3_9_patch3 5_2_3_patch2 5_2_3_patch4 5_3_7 5_3_7_patch4 5_3_3_patch1 5_3_6_patch1 5_3_6 5_3_8_patch3 4_4_2_patch8
+#for CMSSW in 5_3_9_patch1 5_3_7 5_3_8_patch3 4_4_2_patch8
+#for CMSSW in 5_3_10 5_3_10_patch1 5_3_10_patch2 4_4_2_patch8 5_3_9_patch3 5_3_9_patch1 5_3_10
+for CMSSW in 5_3_9_patch3
+
 do
 CMSSWdir=$Castordir/$CMSSW
-for dataset in `nsls $CMSSWdir`
+##New command for EOS
+for dataset in `eos ls $CMSSWdir`;
+#for dataset in `eos ls $CMSSWdir | grep "Neutrino"`;
+#for dataset in `eos ls $CMSSWdir | grep "Neutrino"`;
+#for dataset in `eos ls $CMSSWdir |grep "MinimumBias__Run2012C-26Nov2012-v2"`
+#for dataset in HcalNZS__Run2012B-laserHCALskim_534p2_24Oct2012-v1__DQM 
+# ##Old command for CASTOR
+# # for dataset in `nsls $CMSSWdir`;
+# #for dataset in `nsls $CMSSWdir | grep "TprimeTprimeToTHTH_M-500"`;
+# #for dataset in SingleMu__Run2012B-13Jul2012-v1__DQM MinimumBias__Run2012B-13Jul2012-v1__DQM JetHT__Run2012B-13Jul2012-v1__DQM
 do
 datasetdir=$CMSSWdir/$dataset
 echo $CMSSW ":" $dataset
-for run in `nsls $datasetdir`;
+# for run in `nsls $datasetdir`;
+for run in `eos ls $datasetdir`;
 do
 rundir=$datasetdir/$run
-for nevents in `nsls $rundir`;
+# for nevents in `nsls $rundir`;
+for nevents in `eos ls $rundir`;
 do
 neventsdir=$rundir/$nevents
-for section in `nsls $neventsdir`;
+# for section in `nsls $neventsdir`;
+for section in `eos ls $neventsdir`;
 do
 sectiondir=$neventsdir/$section
-for file in `nsls $sectiondir`;
+# for file in `nsls $sectiondir`;
+for file in `eos ls $sectiondir`;
 do
+echo $file | grep -q $Castordir
+if [ $? -eq 0 ]
+then
+rootfile=$file
+else
 rootfile=$sectiondir/$file
-size=`rfstat $rootfile | grep Size | perl -pe 's/Size \(bytes\)    \: //'`
+fi
+#size=`rfstat $rootfile | grep Size | perl -pe 's/Size \(bytes\)    \: //'`
+size=`eos ls -l $rootfile | awk '{print $5}'`
+
 if [ $size -ne 0 ];
 then
 ## Definition of ffile changed due to upgrade to crab 2_7_5
@@ -59,22 +98,40 @@ ffile=DQM_V0$(echo $rootfile | perl -pe 's/.*\/DQM_V0// ; s/__DQM.*/__DQM.root/'
 file_test=`grep -c "$ffile" upload_bookkeeping.txt`
 if [ $file_test -eq 0 ];
 then
-rfcp $rootfile /tmp/$ffile
+# xrdcp "root://castorcms/"$rootfile"?svcClass=t1transfer" /tmp/$ffile
+xrdcp "root://eoscms.cern.ch/"$rootfile"?svcClass=t1transfer" /tmp/$ffile
+
 if [ `echo $?` != 0 ];
 then
 break
 else
-../VisMonitoring/DQMServer/scripts/visDQMUpload https://cmsweb.cern.ch/dqm/offline /tmp/$ffile
+echo $i | grep -q data
+if [ $? -eq 0 ] ; then
+    echo $CMSSW | grep -q hlt
+    if [ $? -eq 0 ] ; then 
+	server="https://cmsweb.cern.ch/dqm/relval"
+    fi
+    echo $CMSSW | grep -q Commisioning
+    if [ $? -eq 0 ] ; then
+	server="https://cmsweb.cern.ch/dqm/relval"
+    else
+	server="https://cmsweb.cern.ch/dqm/offline"
+    fi
+else
+    server="https://cmsweb.cern.ch/dqm/relval"
+fi
+# server="https://cmsweb.cern.ch/dqm/relval"
+../VisMonitoring/DQMServer/scripts/visDQMUpload $server /tmp/$ffile
 if [ `echo $?` != 0 ];
 then
 echo "------------------------------------------------------------------------------------------------"
-echo /tmp/$ffile could not be uploaded
+echo /tmp/$ffile could not be uploaded to $server
 echo "------------------------------------------------------------------------------------------------"
-rm -f /tmp/$ffile
+rm /tmp/$ffile
 break
 else
 echo "------------------------------------------------------------------------------------------------"
-echo $ffile is uploaded
+echo $ffile is uploaded to $server
 echo "------------------------------------------------------------------------------------------------"
 echo $ffile >> upload_bookkeeping.txt
 
@@ -87,7 +144,7 @@ echo $dataset
 fi
 
 fi
-rm -f $ffile
+rm /tmp/$ffile
 fi
 fi
 fi
@@ -99,9 +156,7 @@ done
 done
 done
 
-cp upload_bookkeeping.txt /afs/cern.ch/user/n/npietsch/harvesting_backup/upload_bookkeeping_backup.txt
-
-cp dataset_bookkeeping.txt /afs/cern.ch/user/n/npietsch/harvesting_backup/dataset_bookkeeping_backup.txt
+#cp upload_bookkeeping.txt /afs/cern.ch/user/n/npietsch/harvesting_backup/upload_bookkeeping_backup.txt
 
 echo "done upload"
 echo
